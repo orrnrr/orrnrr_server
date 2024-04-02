08cc8bdf5a49092433d547cee4feb3140b106acb
minhyeok Hwang <koey2182@etomato.com>
2024-04-02 18:29:26 +0900

    Refactor: api 테스트 성공

---
+++ TokenSourceProvider/JsonObjects/Pm10JsonObj.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenSourceProvider.JsonObjects
+{
+    public record Pm10JsonObj
+    {
+
+        public required Pm10Response response { get; init; }
+    }
+
+    public record Pm10Response
+    {
+        public required Pm10Body body { get; init; }
+        public required Pm10Header header { get; init; }
+    }
+
+    public record Pm10Header
+    {
+        public required string resultMsg { get; init; }
+        public required string resultCode { get; init; }
+    }
+
+    public record Pm10Body
+    {
+        public required int totalCount { get; init; }
+        public required IEnumerable<Pm10Item> items { get; init; }
+        public required int pageNo { get; init; }
+        public required int numOfRows { get; init; }
+    }
+
+    public record Pm10Item
+    {
+        public required string daegu { get; init; }
+        public required string chungnam { get; init; }
+        public required string incheon { get; init; }
+        public required string daejeon { get; init; }
+        public required string gyeongbuk { get; init; }
+        public required string sejong { get; init; }
+        public required string gwangju { get; init; }
+        public required string jeonbuk { get; init; }
+        public required string gangwon { get; init; }
+        public required string ulsan { get; init; }
+        public required string jeonnam { get; init; }
+        public required string seoul { get; init; }
+        public required string busan { get; init; }
+        public required string jeju { get; init; }
+        public required string chungbuk { get; init; }
+        public required string gyeongnam { get; init; }
+        public required string dataTime { get; init; }  // yyyy-MM-dd
+        public required string dataGubun { get; init; }
+        public required string gyeonggi { get; init; }
+        public required string itemCode { get; init; }
+    }
+}
--- TokenSourceProvider/Program.cs
+++ TokenSourceProvider/Program.cs
@@ -1,87 +1,44 @@
 ﻿
 using DataAccessLib.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
 using Newtonsoft.Json;
-using System.Net;
-using TokenSourceProvider.DTOs;
+using TokenSourceProvider.JsonObjects;
 
 namespace TokenSourceProvider
 {
     internal class Program
     {
+
         internal static OrrnrrContext OrrnrrContext { get; } = new OrrnrrContext();
         static async Task Main(string[] args)
         {
-            string json = "{\"Property1\":\"Value1\",\"Property2\":{\"NestedProperty1\":\"NestedValue1\",\"NestedProperty2\":{\"DeepProperty1\":\"DeepValue1\",\"DeepProperty2\":\"DeepValue2\"}}}";
-
-            // JSON 문자열을 객체 A로 역직렬화
-            var objA = JsonConvert.DeserializeObject<ObjectA>(json);
-            var tempJson = JsonConvert.SerializeObject(objA);
-            Console.WriteLine(tempJson);
-            Console.WriteLine(json);
-            Console.WriteLine(json.Equals(tempJson));
-
-            // 역직렬화된 객체 출력
-            Console.WriteLine(objA?.Property1); // Output: Value1
-            Console.WriteLine(objA?.Property2?.NestedProperty1); // Output: NestedValue1
-            Console.WriteLine(objA?.Property2?.NestedProperty2?.DeepProperty1); // Output: DeepValue1
-            Console.WriteLine(objA?.Property2?.NestedProperty2?.DeepProperty2); // Output: DeepValue2
+            await apiTest();
         }
 
+        private static async Task apiTest()
+        {
+            var tokenSource = OrrnrrContext.TokenSources.FirstOrDefault() ?? throw new NullReferenceException("토큰소스 데이터를 찾을 수 없습니다.");
 
+            var url = tokenSource.RequestUrl;
 
+            var request = new HttpClient();
 
-        //var tokenSource = OrrnrrContext.TokenSources.FirstOrDefault() ?? throw new NullReferenceException("토큰소스 데이터를 찾을 수 없습니다.");
+            using var response = await request.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new InvalidOperationException($"api 요청 결과 실패했습니다. 상태코드: {response.StatusCode}, 메세지: {response.Content}");
+            }
 
-        //    var url = tokenSource.RequestUrl;
+            var json = response.Content.ReadAsStringAsync().Result;
+            if (json is null)
+            {
+                throw new InvalidOperationException("api 요청 결과 데이터를 성공적으로 불러오지 못했습니다.");
+            }
 
-        //    var request = new HttpClient();
+            Console.WriteLine(json);
 
-        //    using var response = await request.GetAsync(url);
-        //    if (!response.IsSuccessStatusCode)
-        //    {
-        //        throw new InvalidOperationException($"api 요청 결과 실패했습니다. 상태코드: {response.StatusCode}, 메세지: {response.Content}");
-        //    }
-
-        //    var json = response.Content.ReadAsStringAsync().Result;
-        //    if (json is null)
-        //    {
-        //        throw new InvalidOperationException("api 요청 결과 데이터를 성공적으로 불러오지 못했습니다.");
-        //    }
-
-        //    Console.WriteLine(json);
-            
-        //    var data = JsonConvert.DeserializeObject<Pm10Dto>(json);
-        //    var temp = System.Text.Json.JsonSerializer.Deserialize<Pm10Dto>(json);
-
-        //    Console.WriteLine(JsonConvert.SerializeObject(data));
-        //    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(temp));
-
-        //    var temp1 = data ?? throw new InvalidCastException("data 캐스트 실패");
-        //    //var items = data?.response?.body?.items ?? throw new InvalidOperationException("Json을 객체로 제대로 변환하지 못했습니다.");
-        //    //var items = data?.GetItems();
-        //    //foreach (var item in items)
-        //    //{
-        //    //    Console.WriteLine(item.seoul);
-        //    //}
-        //}
-    }
-    public record ObjectC
-    {
-        public required string DeepProperty1 { get; set; }
-        public required string DeepProperty2 { get; set; }
-    }
-
-    public record ObjectB
-    {
-        public required string NestedProperty1 { get; set; }
-        public required ObjectC NestedProperty2 { get; set; }
-    }
-
-    public record ObjectA
-    {
-        public required string Property1 { get; set; }
-        public required ObjectB Property2 { get; set; }
+            var data = JsonConvert.DeserializeObject<Pm10JsonObj>(json);
+            var tempJson = JsonConvert.SerializeObject(data);
+            Console.WriteLine(tempJson);
+        }
     }
 }
