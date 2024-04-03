f30d584aec4a1626867931043ec206e042fb3a3b
minhyeok Hwang <koey2182@etomato.com>
2024-04-04 07:44:18 +0900

    Revert "Revert "Feat: 토큰소스 생성 api 구현 완료""
    This reverts commit 4fb359b16166d9a671d34dcd85bd851f5f6852aa.

---
+++ OrrnrrWebApi/Responses/TokenSourceResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OrrnrrWebApi.Responses
+{
+    public class TokenSourceResponse
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; init; }
+        [JsonPropertyName("name")]
+        public required string Name { get; init; }
+    }
+}
